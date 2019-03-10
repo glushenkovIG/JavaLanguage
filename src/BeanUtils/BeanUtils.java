0d15136c28c6464aded16e583a344da409f1cf9a
bibloman <glushenkov.ig@phystech.edu>
2019-03-10 08:08:00 +0300

    The task.

---
+++ src/BeanUtils/BeanUtils.java
@@ -0,0 +1,34 @@
+package BeanUtils;
+
+public class BeanUtils {
+
+    /**
+     * Scans object "from" for all getters. If object "to"
+     * <p>
+     * contains correspondent setter, it will invoke it
+     * <p>
+     * to set property value for "to" which equals to the property
+     * <p>
+     * of "from".
+     * <p>
+     * <p/>
+     * <p>
+     * The type in setter should be compatible to the value returned
+     * <p>
+     * by getter (if not, no invocation performed).
+     * <p>
+     * Compatible means that parameter type in setter should
+     * <p>
+     * be the same or be superclass of the return type of the getter.
+     * <p>
+     * <p/>
+     * <p>
+     * The method takes care only about public methods.
+     *
+     * @param to   Object which properties will be set.
+     * @param from Object which properties will be used to get values.
+     */
+
+    public static void assign(Object to, Object from) {
+    }
+}
